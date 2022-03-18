b64bf5e39556696258897487ee292836d22ecc36
DTurner94 <turnerdandre23@gmail.com>
2022-03-18 15:24:11 -0400

    NotesAdded

---
+++ main.js
@@ -0,0 +1,22 @@
+// let, const
+
+//Let - You can reassign values//
+
+//Let Examples//
+
+/*let score = 30;
+
+score = 30
+
+console.log(score);*/
+
+//DATA TYPES- Strings, Numbers, Boolean, null, undefined//
+
+/*String*/ const name = 'John';
+/*Number*/ const age = 30;
+/*Number (decimal)*/ const rating = 4.5;
+/*Boolean*/ const isCool = true;
+/* null*/ const x = null;
+/* undefined */ const y = undefined;
+
+console.log(typeof isCool);
