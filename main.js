84dd28eccf01233f207725d692faca8f09fb7ef6
DTurner94 <turnerdandre23@gmail.com>
2022-03-18 17:44:46 -0400

    htmlCss

--- main.js
+++ main.js
@@ -204,8 +204,45 @@ function addNums(num1,num2) {
 
 addNums(5,4);
 
+//Constructor Function
+function Person(firstName,lastName,dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+    this.getBirthYear = function() {
+        return this.dob.getFullYear();
+    }
+    this.getFullName = function(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
 
+//Class
+class Person1 {
+    constructor(firstName,lastName,dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);    
+    }
+    
+    getBirthYear () {
+        return this.dob.getFullYear();
+    }
 
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+//Instantiate object
+const person1 = new Person('John', 'Doe', '4-3-1990');
+const person2 = new Person('Granny', 'Smith', '4-4-1990');
+
+//console.log(person1.getBirthYear());
+//console.log(person1.getFullName());
+
+console.log(person2.getFullName());
+console.log(person1);
 
 
 
