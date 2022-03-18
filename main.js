018392f1c07c55ac8b7c9c2b6a33556513a3b2a5
DTurner94 <turnerdandre23@gmail.com>
2022-03-18 16:30:38 -0400

    moreNotes

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ const s = 'Hello World!';
 
 console.log(s.toUpperCase());
 console.log(s.substring(0,5).toUpperCase());
-//console.log(s.split(''));
+console.log(s.split(''));
 
 //creating arrays from strings
 const a = 'technology, computers, it, code';
@@ -42,4 +42,110 @@ console.log(a.split(', '));
 //Arrays - variables that hold multiple values
 
 const fruits = ['apples', 'oranges', 'pears'];
+
+fruits[3] = 'grapes';
+
+//add Arrays to end
+fruits.push('mangos');
+
+//add Arrays to front
+fruits.unshift('Pineapples');
+
+//Take last Array off
+fruits.pop();
+
+//check to see if something is a Array
+console.log(Array.isArray(fruits));
+
 console.log(fruits);
+
+//Object Literals
+const person = {
+    firstName: 'Andre',
+    lastName: 'Turner',
+    age: 28,
+    hobbies: ['music', 'video games', 'sports'],
+}
+
+console.log(person.firstName, person.lastName);
+console.log(person.hobbies[1]);
+
+//Add properties to object literals
+person.email = 'turnerdandre23@gmail.com';
+console.log(person);
+
+//Creates Variables in object literals - Advanced
+const { firstName, lastName } = person;
+console.log(firstName, lastName);
+
+//Creating Array of objects
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with Boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist Appt',
+        isCompleted: false
+    },
+];
+
+console.log(todos);
+console.log(todos[1].text);
+
+//Loops
+
+//FOR Loop
+for(let i = 0; i <= 10; i++) {
+  console.log(`For Loop Number: ${i}`);
+}
+
+//WHILE Loops
+let i = 0;
+while(i < 10) {
+    console.log(`While Loop Number: ${i}`);
+    i++;
+}
+
+//LOOP through Arrays
+for(let todo of todos) {
+    console.log(todo.text);
+}
+
+/* HIGH ORDER ARRAY METHODS
+1. forEach - Loops through arrays
+2. map- Allow us to create a new array from an array
+3. filter - allow us to create a new array based on a conditon*/
+
+//forEach
+todos.forEach(function(todo) {
+  console.log(todo.text);
+});
+
+//Map
+const todoText = todos.map(function(todo) {
+   return todo.text;
+});
+
+console.log(todoText);
+
+//Filter
+const todoCompleted = todos.filter(function(todo) {
+  return todo.isCompleted === true;
+});
+
+console.log(todoCompleted);
+
+
+
+
+
+
+
